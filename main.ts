3f52cfb9c6f2d8f6294c367bf0a40703f1c3eac3
Stephan Ango <10565871+kepano@users.noreply.github.com>
2023-07-13 16:42:32 -0700

    Initial version

--- main.ts
+++ main.ts
@@ -1,81 +1,67 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, FrontMatterCache, parseFrontMatterEntry, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface OpenerSettings {
+	keyName: string;
+	devUrl: string;
+	prodUrl: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: OpenerSettings = {
+	keyName: 'permalink',
+	devUrl: 'http://127.0.0.1:4000',
+	prodUrl: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export const openUrl = (url: string) => {
+  window.open(url);
+};
+
+export default class Opener extends Plugin {
+	settings: OpenerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'permalink-open-dev-url',
+			name: 'Open page on development site',
+
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				let file = app.workspace.getActiveFile();
+				let metadata = this.app.metadataCache.getFileCache(file)?.frontmatter;
+				let name = file.basename;
+				let permalink = parseFrontMatterEntry(metadata, this.settings.keyName);
+				if (!permalink) {
+					console.log('No permalink found, using filename: ' + name);
+					permalink = name.toLowerCase().split(' ').join('-');
 				}
+
+				let url = this.settings.devUrl.replace(/\/+$/, '') + '/' + permalink.replace(/^\/+/, '');
+				openUrl(url);
+				console.log('Opening dev site: ' + url);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'permalink-open-prod-url',
+			name: 'Open page on live site',
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			callback: () => {
+				let file = app.workspace.getActiveFile();
+				let metadata = this.app.metadataCache.getFileCache(file)?.frontmatter;
+				let name = file.basename;
+				let permalink = parseFrontMatterEntry(metadata, this.settings.keyName);
+				if (!permalink) {
+					console.log('No permalink found, using filename: ' + name);
+					permalink = name.toLowerCase().split(' ').join('-');
+				}
+				let url = this.settings.prodUrl.replace(/\/+$/, '') + '/' + permalink.replace(/^\/+/, '');
+				openUrl(url);
+				console.log('Opening live site: ' + url);
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new OpenerSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,26 +77,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class OpenerSettingTab extends PluginSettingTab {
+	plugin: Opener;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Opener) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +90,39 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		new Setting(containerEl)
+			.setName('Permalink property name')
+			.setDesc('The file property used to populate the page slug')
+			.addText(text => text
+				.setPlaceholder('permalink')
+				.setValue(this.plugin.settings.keyName)
+				.onChange(async (value) => {
+					console.log('Key name: ' + value);
+					this.plugin.settings.keyName = value;
+					await this.plugin.saveSettings();
+				}));
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Live site base URL')
+			.setDesc('The production URL for your site')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('http://')
+				.setValue(this.plugin.settings.prodUrl)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('Prod URL: ' + value);
+					this.plugin.settings.prodUrl = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Development site base URL')
+			.setDesc('The staging URL for your site')
+			.addText(text => text
+				.setPlaceholder('http://')
+				.setValue(this.plugin.settings.devUrl)
+				.onChange(async (value) => {
+					console.log('Dev URL: ' + value);
+					this.plugin.settings.devUrl = value;
 					await this.plugin.saveSettings();
 				}));
 	}
